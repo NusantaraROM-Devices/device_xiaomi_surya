f86e6dec4b3679708e27c659ab3db806a23175f7
Adrian Roos <roosa@google.com>
2018-08-20 16:13:04 +0200

    surya: Add overlay for masking the notch
    Bug: 112876936
    Test: Go to Settings > Developer Options > Simulate display with cutout > "No cutout". Cutout should be hidden. Rotate screen, take screenshots, screenrecord should all work.
    Change-Id: I66684d8254fd3c1ee6851e67c0b09d176d4c02af
    (cherry picked from commit dcc48dc9071c37c8b9fe7caee96585d20d149060)
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: agustkid <agustkid@gmail.com>

--- device.mk
+++ device.mk
@@ -125,7 +125,8 @@ PRODUCT_PACKAGES += \
     FrameworksResCommon \
     FrameworksResTarget \
     DevicesOverlay \
-    DevicesAndroidOverlay
+    DevicesAndroidOverlay \
+    NoCutoutOverlay
 
 # Permissions
 PRODUCT_COPY_FILES += \
