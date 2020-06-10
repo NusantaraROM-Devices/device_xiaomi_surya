7b1daa6599d97977137f0d2f84dbe54199612afb
Akhil Narang <me@akhilnarang.dev>
2020-06-10 10:45:13 +0530

    surya: import keylayout
    Disable DPAD_HOME
    
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>
    Change-Id: Id27e3d1c316f96b5a87328bee68ad23b475556f5
    Signed-off-by: agustkid <agustkid@gmail.com>

--- device.mk
+++ device.mk
@@ -57,6 +57,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom
 
+# Keylayout
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/uinput-fpc.kl \
+    $(LOCAL_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/uinput-goodix.kl
+
 # NFC
 PRODUCT_PACKAGES += \
     com.android.nfc_extras \
