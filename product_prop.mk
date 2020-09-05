34bdfff473404acacd5743407477049cee9668e1
ghostrider-reborn <gh0strider.2k18.reborn@gmail.com>
2020-09-05 17:13:25 +0530

    surya: Enable fluence for audio/voice recording  * Fixes recording in google audio recorder
    Signed-off-by: agustkid <agustkid@gmail.com>

--- product_prop.mk
+++ product_prop.mk
@@ -39,3 +39,8 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.sf.blurs_are_expensive=1 \
     debug.sf.latch_unsignaled=1 \
     debug.sf.disable_backpressure=1
+
+# Recorder
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.vendor.audio.fluence.voicerec=true \
+    persist.vendor.audio.fluence.audiorec=true
