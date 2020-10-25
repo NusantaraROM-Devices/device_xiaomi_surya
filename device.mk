7f2d9bc5fb65049f3f4703bbf66ec30228b0c24f
xyyx <xyyx@mail.ru>
2020-10-25 05:43:12 +0300

    surya: Add FM support
    Signed-off-by: Tushar Mahajan <mahajant99@gmail.com>
    Signed-off-by: agustkid <agustkid@gmail.com>

--- device.mk
+++ device.mk
@@ -57,7 +57,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fastbootd
 
-# fstab
+# FM
+PRODUCT_PACKAGES += \
+    FM2 \
+    libqcomfm_jni \
+    qcom.fmradio
+
+# Fstab
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom
 
