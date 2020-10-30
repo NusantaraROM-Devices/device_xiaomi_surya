50defff4cc1d54803cf91aff7fc8ae916f04da8a
Lostark13 <tushar9915@gmail.com>
2020-10-30 17:12:21 +0530

    surya: Update Fluence Props
    * fixes google recorder

--- product_prop.mk
+++ product_prop.mk
@@ -31,5 +31,6 @@ PRODUCT_PRODUCT_PROPERTIES += \
 
 # Recorder
 PRODUCT_PRODUCT_PROPERTIES += \
+    ro.vendor.audio.sdk.fluencetype=fluence \
     persist.vendor.audio.fluence.voicerec=true \
     persist.vendor.audio.fluence.audiorec=true
