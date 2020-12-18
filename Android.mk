449abe001fb0546d1a406c99a796821f676a64a4
Lostark13 <tushar9915@gmail.com>
2020-12-18 12:27:27 +0530

    surya: Add symlink for WFD libs
    Signed-off-by: Lostark13 <tushar9915@gmail.com>
    Signed-off-by: Tushar Mahajan <mahajant99@gmail.com>
    Signed-off-by: agustkid <agustkid@gmail.com>

--- Android.mk
+++ Android.mk
@@ -21,4 +21,14 @@ $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)
 
 include $(CLEAR_VARS)
 
+WFD_LIB := libwfdnative.so
+WFD_SYMLINKS := $(addprefix $(TARGET_OUT_APPS_PRIVILEGED)/WfdService/lib/arm64/,$(notdir $(WFD_LIB)))
+$(WFD_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "WFD lib link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /system/lib64/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(WFD_SYMLINKS)
+
 endif
