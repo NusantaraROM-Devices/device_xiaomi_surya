426c70b699d66945678c8f714b9902974bb2c08e
GrettoRosella <rezarifaldy12@gmail.com>
2021-03-03 19:44:57 +0700

    surya: switch to sunfish march build fingerprint

--- init/init_surya.cpp
+++ init/init_surya.cpp
@@ -95,8 +95,8 @@ void set_device_props(const string fingerprint, const string description,
 void load_device_properties() {
     string hwname = GetProperty("ro.boot.hwname", "");
 
-    string fingerprint = "google/sunfish/sunfish:11/RQ1A.210105.002/6985033:user/release-keys";
-    string description = "sunfish-user 11 RQ1A.210105.002 6985033 release-keys";
+    string fingerprint = "google/sunfish/sunfish:11/RQ2A.210305.006/7119741:user/release-keys";
+    string description = "sunfish-user 11 RQ2A.210305.006 7119741 release-keys";
 
     if (hwname == "surya") {
         set_device_props(fingerprint, description, "Poco", "surya", "M2007J20CG");
