a63d796a555b21d1e956cacfa9e8657839993db2
agustkid <agustkid@gmail.com>
2021-06-16 19:40:54 +0700

    surya : corrected lights service hals
    Signed-off-by: agustkid <agustkid@gmail.com>

--- device.mk
+++ device.mk
@@ -97,7 +97,7 @@ PRODUCT_COPY_FILES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.surya
+    android.hardware.light@2.0-service.xiaomi_surya
 
 # NFC
 PRODUCT_PACKAGES += \
