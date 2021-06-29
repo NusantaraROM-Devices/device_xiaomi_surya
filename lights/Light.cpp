6c54c8112e8a2281fd8e668cf245e1b985a59ab4
Genkzsz11 <Genkzsz11@gmail.com>
2021-06-29 17:00:30 +0700

    Revert "surya: light: Properly scale brightness range"
    This reverts commit c1eb5cc56274bbd651cb484c4b2df3055c7e50da.

--- lights/Light.cpp
+++ lights/Light.cpp
@@ -93,7 +93,7 @@ static uint32_t getBrightness(const LightState& state) {
 }
 
 static inline uint32_t scaleBrightness(uint32_t brightness, uint32_t maxBrightness) {
-    return (brightness - 1) * (maxBrightness - 1) / (0xFF - 1) + 1;
+    return brightness * maxBrightness / 0xFF;
 }
 
 static inline uint32_t getScaledBrightness(const LightState& state, uint32_t maxBrightness) {
