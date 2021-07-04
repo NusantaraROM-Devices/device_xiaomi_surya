feedd8baf4aacea53eb409e349d4fa4201d2de85
rahul9999xda <rahul9999xda@gmail.com>
2021-07-04 12:40:10 +0000

    surya: Add prebuilt GoogleCameraGo
    Signed-off-by: rahul9999xda <rahul9999xda@gmail.com>
    Signed-off-by: David Setiawan <valkry24@gmail.com>
    [azrim: Update camera app to CameraGo_2.5_Greatness_Special.apk (com.google.android.apps.cameralite)]
    Signed-off-by: azrim <mirzaspc@gmail.com>

--- device.mk
+++ device.mk
@@ -114,7 +114,8 @@ PRODUCT_PACKAGES += \
     libshim_megvii
 
 PRODUCT_PACKAGES += \
-    Snap
+    Snap \
+    GoogleCameraGo
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
