7f139913f486cfc2fd0b5790fe95a5f9c66e3731
Magicxavi <rolfpeterkesche@gmail.com>
2021-11-12 22:29:40 +0530

    surya: fingerprint: Add on show\hide udfps overlays
    Without this, build will throw error below:
    
    device/xiaomi/raphael/fingerprint/BiometricsFingerprint.cpp:209:25: error: allocating an object of abstract class type 'android::hardware::biometrics::fingerprint::V2_3::implementation::BiometricsFingerprint'
            sInstance = new BiometricsFingerprint();
                            ^
    out/soong/.intermediates/hardware/interfaces/biometrics/fingerprint/2.3/android.hardware.biometrics.fingerprint@2.3_genc++_headers/gen/android/hardware/biometrics/fingerprint/2.3/IBiometricsFingerprint.h:214:47: note: unimplemented pure virtual method 'onShowUdfpsOverlay' in 'BiometricsFingerprint'
        virtual ::android::hardware::Return<void> onShowUdfpsOverlay() = 0;
                                                  ^
    out/soong/.intermediates/hardware/interfaces/biometrics/fingerprint/2.3/android.hardware.biometrics.fingerprint@2.3_genc++_headers/gen/android/hardware/biometrics/fingerprint/2.3/IBiometricsFingerprint.h:219:47: note: unimplemented pure virtual method 'onHideUdfpsOverlay' in 'BiometricsFingerprint'
        virtual ::android::hardware::Return<void> onHideUdfpsOverlay() = 0;
                                                  ^
    since we don't use it, for now let's just satisfy it with blank callbacks.
    
    Signed-off-by: Pranav Temkar <pranavtemkar@gmail.com>
    Co-authored-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: DarkAngelGR <asavvo01@gmail.com>

--- fingerprint/BiometricsFingerprint.h
+++ fingerprint/BiometricsFingerprint.h
@@ -78,6 +78,8 @@ struct BiometricsFingerprint : public IBiometricsFingerprint {
     std::mutex mClientCallbackMutex;
     sp<IBiometricsFingerprintClientCallback> mClientCallback;
     fingerprint_device_t* mDevice;
+    Return<void> onShowUdfpsOverlay() override;
+    Return<void> onHideUdfpsOverlay() override;
 };
 
 }  // namespace implementation
