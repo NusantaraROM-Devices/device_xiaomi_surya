080c70253128eddfd15146b8a86a6d812c1ea044
DestroyerV <95367218+DestroyerV@users.noreply.github.com>
2022-02-14 13:42:46 +0530

    surya: added NeverAllows
    Signed-off-by: Hafizh Arrizqa <hafizharrizqa15@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -8,6 +8,9 @@ BUILD_BROKEN_DUP_RULES := true
 
 DEVICE_PATH := device/xiaomi/surya
 
+#NeverAllows
+SELINUX_IGNORE_NEVERALLOWS := true
+
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
