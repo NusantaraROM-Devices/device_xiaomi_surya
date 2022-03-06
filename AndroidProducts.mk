94153edf1476cf569da153e07fc26d1789879acd
Hafizh Arrizqa <hafizharrizqa15@gmail.com>
2022-03-06 16:50:58 +0000

    surya: Nad-ify
    Signed-off-by: Hafizh Arrizqa <hafizharrizqa15@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2020 The LineageOS Project
+# Copyright (C) 2022 The NusantaraOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_surya.mk
+    $(LOCAL_DIR)/nad_surya.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_surya-user \
-    lineage_surya-userdebug \
-    lineage_surya-eng
+    nad_surya-user \
+    nad_surya-userdebug \
+    nad_surya-eng
